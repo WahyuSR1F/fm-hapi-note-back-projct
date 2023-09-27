43ccfb4e6a05a3c1e0698f773ce3e0246c4954f2
sahrt <sahricy5@gmail.com>
2023-09-28 01:07:06 +0700

    fixs bugs host value

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.Server({
     port: 3000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       // mendambahkan sebuah cors
       // untuk mengizinkan akses dengan origin lain
